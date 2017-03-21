e303f3dce73edbd3a4f82ff5444b9d2b07f51271
Darian Brown <darianbr@gmail.com>
2017-03-21 13:52:39 +1000

    Attribute value is now string instead of mixed.

--- test/suite/unit/Attribute.spec.php
+++ test/suite/unit/Attribute.spec.php
@@ -7,18 +7,18 @@ namespace Rinq;
 describe(Attribute::class, function () {
     describe('->construct()', function () {
         it('should construct a non-frozen attribute', function () {
-            $subject = Attribute::create('foo', ['bar' => 'baz']);
+            $subject = Attribute::create('foo', '{"bar":"baz"}');
 
             expect($subject->key())->to->equal('foo');
-            expect($subject->value())->to->equal(['bar' => 'baz']);
+            expect($subject->value())->to->equal('{"bar":"baz"}');
             expect($subject->isFrozen())->to->be->false();
         });
 
         it('should construct a frozen attribute', function () {
-            $subject = Attribute::freeze('foo', ['bar' => 'baz']);
+            $subject = Attribute::freeze('foo', '{"bar":"baz"}');
 
             expect($subject->key())->to->equal('foo');
-            expect($subject->value())->to->equal(['bar' => 'baz']);
+            expect($subject->value())->to->equal('{"bar":"baz"}');
             expect($subject->isFrozen())->to->be->true();
         });
     });
